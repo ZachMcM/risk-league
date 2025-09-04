160a5c1f44bd9de45c056b63042fc4d6d29f2826
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-09-04 11:56:28 -0400

    batch inserting players by 500 and fix for null positions

--- orchestration/batch/upsert_players.py
+++ orchestration/batch/upsert_players.py
@@ -21,6 +21,8 @@ def main():
                 sys.exit(1)
             provided_leagues.append(sys.argv[i])
 
+        BATCH_SIZE = 500  # Adjust batch size as needed
+
         for league in provided_leagues:
             total_upserted = 0
 
@@ -28,24 +30,34 @@ def main():
             playersData = playersReq.json()
             players = playersData["data"][league]
 
-            for player in players:
+            # Process players in batches
+            batch = []
+            for i, player in enumerate(players):
                 req_body = {
                     "playerId": player["player_id"],
                     "name": player["player"],
                     "teamId": player["team_id"],
-                    "position": player["position"],
+                    "position": player.get("position") or "N/A",
                     "number": player["number"],
                     "height": player["height"],
                     "weight": float(player["weight"]) if player["weight"] else 0,
                     "status": player.get("status", "INACT") or "INACT",
                     "league": league,
                 }
-                post_req = server_req(
-                    route="/players", method="POST", body=json.dumps(req_body)
-                )
-                if post_req.status_code != 304:
-                    logger.info(f"Successfully upserted {player['player']}")
-                    total_upserted += 1
+                batch.append(req_body)
+
+                # Send batch when it reaches BATCH_SIZE or at the end
+                if len(batch) == BATCH_SIZE or i == len(players) - 1:
+                    batch_req = server_req(
+                        route="/players", method="POST", body=json.dumps({ "players": batch })
+                    )
+                    if batch_req.status_code != 304:
+                        total_upserted += len(batch)
+                        logger.info(f"Successfully upserted batch of {len(batch)} players, {len(players) - total_upserted} left")
+                    else:
+                        logger.info(f"Batch of {len(batch)} players already up to date")
+                    
+                    batch = []  # Reset batch
 
             print(f"{total_upserted} {league} players upserted")
 
