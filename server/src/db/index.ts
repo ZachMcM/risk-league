6b35337eccf5dc412f643fdbeee2fdd262c9850f
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-09-18 00:57:30 -0400

    manually pooling

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,7 +2,15 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 import * as relations from "./relations";
+import { Pool } from "pg";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  max: 50, // maximum number of clients in pool
+  idleTimeoutMillis: 20000, // close idle clients after 20 seconds
+  connectionTimeoutMillis: 10000, // timeout when connecting new client
+});
+
+export const db = drizzle(pool, {
   schema: { ...schema, ...relations },
 });
