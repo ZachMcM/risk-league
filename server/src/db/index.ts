ce06feb3d01564c1ea831a27ed011db7ec6c1ec3
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-09-19 10:49:35 -0400

    pooling

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,13 +4,6 @@ import * as relations from "./relations";
 import * as schema from "./schema";
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-  max: 50, // maximum number of clients in pool
-  idleTimeoutMillis: 20000, // close idle clients after 20 seconds
-  connectionTimeoutMillis: 10000, // timeout when connecting new client
-});
-
-export const db = drizzle(pool, {
+export const db = drizzle(process.env.DATABASE_URL!, {
   schema: { ...schema, ...relations },
 });
