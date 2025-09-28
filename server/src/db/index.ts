3cfc3c0015f646e4205f3f4b17ea7f88d274ea17
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-09-27 23:03:12 -0400

    fix

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -6,7 +6,7 @@ import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-  max: 20,
+  max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
