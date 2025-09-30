88f4e917c1047640d07a4c3a03942d2dee4f6f49
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-09-30 00:15:55 -0400

    changes for test flight

--- client/lib/ads.ts
+++ client/lib/ads.ts
@@ -1,5 +1,5 @@
 import { TestIds } from "react-native-google-mobile-ads";
 
 // TODO add change to use actual unit
-export const adaptiveBannerUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : "";
-export const interstitialAdUnitId =  __DEV__ ? TestIds.INTERSTITIAL : ""
+export const adaptiveBannerUnitId = TestIds.ADAPTIVE_BANNER;
+export const interstitialAdUnitId = TestIds.INTERSTITIAL;
